a0e80ed9298b26435ddb22c4529a84bf0acf80ff
nigamelastic <nigamelastic@gmail.com>
2018-11-27 23:16:10 +0100

    increased the loop

--- tinder.sh
+++ tinder.sh
@@ -5,7 +5,7 @@ tell application "Google Chrome"
                 activate
                 open location "https://tinder.com/app/recs"
                 delay 1
-                repeat 30 times
+                repeat 80 times
                 delay 1
                 tell application "System Events"
 
