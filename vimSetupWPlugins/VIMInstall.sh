353f15a922dfdb28f267c61fafed412256d48468
Kshitij Nigam <nigamelastic@gmail.com>
2021-01-13 23:36:47 +0100

    Update VIMInstall.sh

--- vimSetupWPlugins/VIMInstall.sh
+++ vimSetupWPlugins/VIMInstall.sh
@@ -9,3 +9,7 @@ sudo apt-get update
 
 ### installing vim 8.2 as of writing this script ####
 sudo apt-get install vim
+
+
+### installing vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
