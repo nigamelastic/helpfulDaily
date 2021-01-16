a6978e8ceb2a34b27e72a1d546e77f5f9e4c399b
Kshitij Nigam <nigamelastic@gmail.com>
2021-01-16 22:33:28 +0100

    Update VIMInstall.sh

--- vimSetupWPlugins/VIMInstall.sh
+++ vimSetupWPlugins/VIMInstall.sh
@@ -2,13 +2,13 @@
 #!/usr/bin/env bash
 
 #### Remove the already installed tiny vim ####
-sudo apt-get remove vim-tiny
+sudo apt-get remove -y vim-tiny
 
 ### Performing an update ###
-sudo apt-get update
+sudo apt-get update -y
 
 ### Installing vim 8.2 as of writing this script ####
-sudo apt-get install vim
+sudo apt-get install -y vim
 
 ### Installing vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
