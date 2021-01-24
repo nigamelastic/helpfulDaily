359269d0aff09492e36fbd577ee7f722d139ad8b
Kshitij Nigam <nigamelastic@gmail.com>
2021-01-24 22:17:55 +0100

    Update example.sh

--- screenSetup/example.sh
+++ screenSetup/example.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
 echo "Starting the script"
-screen -dmS jeher 
-screen -S jeher -X stuff './timer.sh^M'
-echo "finished check the screen"
+screen -dmS jeher ./timer.sh
+
+#screen -dmS jeher 
+#screen -S jeher -X stuff './timer.sh^M'
+echo "finished"
+echo "use \"screen -r jeher\" --> to resume the screen"
