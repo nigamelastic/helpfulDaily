6445077b664214b924d44ade69d19c384b4e086d
Kshitij Nigam <nigamelastic@gmail.com>
2021-02-02 16:59:26 +0100

    Update VIMInstall.sh

--- vimSetupWPlugins/VIMInstall.sh
+++ vimSetupWPlugins/VIMInstall.sh
@@ -10,13 +10,13 @@ sudo apt-get update -y
 ### Installing vim 8.2 as of writing this script ####
 sudo apt-get install -y vim
 
-### Installing vim-plug
+### adding vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
 
 
-#installing vim-go plugin
+#addding vim-go plugin
 git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 
 ### Adding .vimrc to home directory
@@ -25,3 +25,8 @@ cp vimrc ~/.vimrc
 
 ## Installing pylint via pip
 sudo pip3 install pylint
+
+
+##Installing the plugins
+
+vim +PlugInstall
