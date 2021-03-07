fc4a08fbd7f7cefcec9522257ac6531ceb0db600
Kshitij Nigam <nigamelastic@gmail.com>
2021-03-07 23:01:49 +0100

    Update RSACTFTool.sh

--- CTFtools/RSACTFTool.sh
+++ CTFtools/RSACTFTool.sh
@@ -3,7 +3,7 @@
 #installs RSA CTF TOOl
 git clone https://github.com/Ganapati/RsaCtfTool.git
 cd RsaCtfTool
-sudo apt-get install libgmp3-dev libmpc-dev
+sudo apt-get install -y libgmp3-dev libmpc-dev 
 pip3 install -r "requirements.txt"
 
 
