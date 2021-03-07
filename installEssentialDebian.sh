80a878a429c3b6c2b1d3806a3a4c3667e157a5a2
Kshitij Nigam <nigamelastic@gmail.com>
2021-03-07 21:30:39 +0100

    Update installEssentialDebian.sh

--- installEssentialDebian.sh
+++ installEssentialDebian.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt install tree
-#sudo apt remove tiny-vim && sudo apt install vim
+#!/usr/bin/env bash
+sudo apt update
 
 sudo apt install arp-scan
 
+sudo apt install tree
+sudo apt remove tiny-vim 
+sudo apt install vim
+sudo apt install screen
+
+sudo apt install -y docker.io
