387b6bfabbb4cfa2b2b655ddc9839a11f7c43bf7
Kshitij Nigam <nigamelastic@gmail.com>
2021-12-16 23:46:02 +0100

    Create bulkDelete.sh

---
+++ bulkDelete.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+if [ -z "$1" ]; then
+    echo -e "Usage: $(basename $0) FILE\n"
+    exit 1
+fi
+
+if [ ! -e "$1" ]; then
+    echo -e "$1: File doesn't exist.\n"
+    exit 1
+fi
+
+while read -r line; do
+    [ -n "$line" ] && rm -- "$line"
+done < "$1"
