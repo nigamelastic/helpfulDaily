9f075e9ad56133a1720a4603d19ff6487e985011
nigamelastic <nigamelastic@gmail.com>
2023-03-17 17:59:41 -0400

    adding installnvim

--- installNvim.sh
+++ installNvim.sh
@@ -2,4 +2,5 @@
 sudo apt install neovim -y
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
-
+echo 'eval "$(oh-my-posh init zsh)"'  >> ~/.zshrc
+oh-my-posh font install
