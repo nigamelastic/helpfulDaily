3b2f4a5e40920efdfa08607a4101c91d9632a27a
nigamelastic <nigamelastic@gmail.com>
2023-03-17 17:21:54 -0400

    adding installnvim

--- installNvim.sh
+++ installNvim.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install neovim
+sudo apt install neovim -y
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
+
