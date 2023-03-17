5dfcb0cd9fbdd83db223ebc808622b67d3510560
nigamelastic <nigamelastic@gmail.com>
2023-03-17 18:06:17 -0400

    rename installposh

---
+++ installposh.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#sudo apt install neovim -y
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+echo 'eval "$(oh-my-posh init zsh)"'  >> ~/.zshrc
+oh-my-posh font install
