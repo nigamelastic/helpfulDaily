66126299a5eff7e1bcf52092d250a12ceb4fff39
Kshitij Nigam <nigamelastic@gmail.com>
2023-03-30 22:40:43 +0200

    Update installNvim.sh

--- installNvim.sh
+++ installNvim.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 sudo apt install neovim -y
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
+##  Once done run the following in lvim
+## :Telescope colorscheme
+## select tokyonight theme
